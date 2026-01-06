f7c647fda15bacef6ed3574a92e6513fa8b0a455
imanehajji-tech <imane.hajji@3wa.io>
2026-01-06 13:10:11 +0000

    Sauvegarde exercice 1

---
+++ exercices-boucles-conditions/exercice-1/index.js
@@ -0,0 +1,20 @@
+let i = 0;
+while(i<11)
+{
+    console.log(i);
+    i = i + 1;
+}
+
+let y = 25;
+while (y>=20)
+{
+    console.log(y);
+    y = y-1
+}
+
+let z = 0;
+while (z<=20)
+{
+    console.log(z)
+    z = z+2
+}
